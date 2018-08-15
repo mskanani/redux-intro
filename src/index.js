2d2e8db0d2a5d816bfa4a4ebb72120bbd891bb9d
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-15 20:52:45 +0400

    Connecting the Store to React

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore } from 'redux'; // function
 import reducer from './store/reducer';
+import { Provider } from 'react-redux'; // component with Capital letter
 
 //before mounting the app, we create store
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
