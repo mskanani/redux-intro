4b04f15e98ce57e284e35b0dafff954f8825bffe
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-15 21:13:53 +0400

    Dispatching more actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,21 @@ const reducer = (state = initialState, action) => {
             counter: state.counter+1
         }
     }
+    if(action.type === 'DECREMENT') { // the convention is to use upper case character
+        return {
+            counter: state.counter-1
+        }
+    }
+    if(action.type === 'ADD') {
+        return {
+            counter: state.counter+10
+        }
+    }
+    if(action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter-8
+        }
+    }
     return state;
 };
 
