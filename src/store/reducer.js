f6fded12a807de58b64a4097654ab4f900be5891
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-16 22:45:24 +0400

    Switch-Case in the Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,25 +3,23 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter+1
-        }
-    }
-    if(action.type === 'DECREMENT') { // the convention is to use upper case character
-        return {
-            counter: state.counter-1
-        }
-    }
-    if(action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-    if(action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
+    switch (action.type) {
+        case 'INCREMENT': // the convention is to use upper case character
+            return {
+                counter: state.counter+1
+            } //no need for break because return automatically exit out of this function
+        case 'DECREMENT':
+            return {
+                counter: state.counter-1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.value
+            }
     }
     return state;
 };
