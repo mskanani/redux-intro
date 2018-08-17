f639be5f9ce0539979cd7a5c33c8ec9bbf4860f0
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-17 23:10:21 +0400

    Updating State Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => { 
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -31,6 +39,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', value: 10}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 15}),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' }),
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,34 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT': // the convention is to use upper case character
             return {
+                ...state,
                 counter: state.counter+1
             } //no need for break because return automatically exit out of this function
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter-1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(),value: state.counter}) //immutable way of updating an array by adding a new item
             }
     }
     return state;
